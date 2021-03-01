ec73f4d323d6e12bb9cad5b9cf3b18b1b674f42e
ammar-64 <ammar.n.sammour@gmail.com>
2021-03-01 16:50:23 +0300

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+  attr_accessor :first_length, :seconed_length, :third_length
+
+  def initialize(first_length , seconed_length , third_length )
+    @first_length = first_length
+    @seconed_length = seconed_length
+    @third_length = third_length    
+  end
+
+  def kind
+    if @first_length + @seconed_length <= @third_length || @first_length + @third_length <= @seconed_length || @seconed_length + @third_length <= @first_length
+      raise TriangleError   
+    else
+      if @first_length == @seconed_length && @seconed_length == @third_length
+        :equilateral
+      elsif @first_length == @seconed_length || @seconed_length == @third_length || @first_length == @third_length
+        :isosceles
+      else
+        :scalene
+      end  
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
